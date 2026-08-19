ecd0961cdc17f5eb2a0dd21b1a2f22f5dcd587ac
AhenkanYeboah <138991855+AhenkanYeboah@users.noreply.github.com>
2026-08-19 12:01:14 +0000

    Update PlatformAdmin.php

--- app/Models/PlatformAdmin.php
+++ app/Models/PlatformAdmin.php
@@ -22,7 +22,7 @@ class PlatformAdmin extends Authenticatable
     protected function casts(): array
     {
         return [
-            // Removed 'password' => 'hashed' to prevent conflict with manual Hash::check()
+            'password' => 'hashed',
         ];
     }
 }
